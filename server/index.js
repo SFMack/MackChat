09e8c64b1cd64a00bbc182fc353dd8f587c7edb3
SFMack <sfcamack@icloud.com>
2020-03-24 22:31:36 -0700

    infobar added along with styling adjustments

--- server/index.js
+++ server/index.js
@@ -27,6 +27,30 @@ io.on('connection', socket => {
 		const { error, user } = addUser({ id: socket.id, name, room });
 
 		if (error) return callback(error);
+
+		// sends a welcome message to the user
+		socket.emit('message', {
+			user: 'admin',
+			text: `${user.name}, welcome to the ${user.room} room`
+		});
+
+		// sends a message to everyone (except the joining user)
+		// to let them know who is joining the room
+		socket.broadcast
+			.to(user.room)
+			.emit('message', { user: 'admin', text: `${user.name}, has joined!` });
+
+		socket.join(user.room);
+
+		callback();
+	});
+
+	socket.on('sendMessage', (message, callback) => {
+		const user = getUser(socket.id);
+
+		io.to(user.room).emit('message', { user: user.name, text: message });
+
+		callback();
 	});
 
 	socket.on('disconnect', () => {
